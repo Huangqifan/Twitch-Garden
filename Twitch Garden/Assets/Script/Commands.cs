806bbaf387f93b49c6ce4633f8db85dc180c5f9c
Jonathan Hopkins <jwhopkins@ucdavis.edu>
2019-05-19 12:26:07 -0700

    Added animation controllers

--- Twitch Garden/Assets/Script/Commands.cs
+++ Twitch Garden/Assets/Script/Commands.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Commands : MonoBehaviour
 {
+    [SerializeField] Animator tree_animator;
 	public GameObject Weeds;
 	public GameObject Flowers; 
 	public Sprite FlowerSprite;
